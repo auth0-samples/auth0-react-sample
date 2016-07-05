8edc8ef4b79760e94f8f83a07353be3ff2520cb6
Flavio Granero <maltempe@gmail.com>
2016-07-05 18:23:05 -0300

    adding 04-linking-accounts with LinkAccountService and LinkedAccountsList/ListAccountItem components

---
+++ 04-Linking-Accounts/src/views/Main/routes.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Route, IndexRedirect} from 'react-router'
+import AuthService from 'utils/AuthService'
+import Container from './Container'
+import Home from './Home/Home'
+import Login from './Login/Login'
+
+const auth = new AuthService(__AUTH0_CLIENT_ID__, __AUTH0_DOMAIN__);
+
+// onEnter callback to validate authentication in private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+    <Route path="/" component={Container} auth={auth}>
+      <IndexRedirect to="/home" />
+      <Route path="home" component={Home} onEnter={requireAuth} />
+      <Route path="login" component={Login} />
+      <Route path="access_token=:token" component={Login} /> //to prevent router errors
+    </Route>
+  )
+}
+
+export default makeMainRoutes
